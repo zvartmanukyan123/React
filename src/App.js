a363dd9d983d471fe8410737757cebf24d5a2999
Home <home@Homes-MBP.lan>
2019-07-10 22:29:58 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Հա ի՞նչ կար ստեղ դժվար Հարութ ջան։
         </p>
         <a
           className="App-link"
